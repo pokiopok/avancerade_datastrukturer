dc1a00a33c7c794bbf10ec6949f8dda1f0f505ad
Piikkio <christianapokio@gmail.com>
2024-03-06 15:39:02 +0100

    gpt yipyip

--- src/test/java/TreapTest.java
+++ src/test/java/TreapTest.java
@@ -4,8 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TreapTest {
 
+    Treap<Integer> treap = new Treap<>();
+    Integer[] sorted = new Integer[]{1,2,3,4,5,6,7,8,9};
     @Test
     void insert() {
+        for(Integer i : sorted){
+            treap.insert(i);
+        }
+        assertTrue(treap.contains(3));
     }
 
     @Test
